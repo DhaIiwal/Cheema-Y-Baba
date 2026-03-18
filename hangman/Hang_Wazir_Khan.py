1cd902274ccc00b575e7feb62cbaee3e1778b992
DhaIiwal <edhaliwal@gobindsarvar.ca>
2026-03-18 10:55:19 -0700

    hangmain

--- hangman/Hang_Wazir_Khan.py
+++ hangman/Hang_Wazir_Khan.py
@@ -1,26 +1,48 @@
 import random
-word_list = ["aurangzeb", "modi", "indra gandhi", "wazir khan", "abdali"]
+from hangmanwords import word_list
+from hangman_ARt import stages, logo
 
-
+print(logo)
+lives = 6
 chosen_word = random.choice(word_list)
-placeholder = ""
-display = ""
+
+correct_letters =[]
+guessed_letters = []
+
 is_game_over = False
 while not is_game_over:
     guess = input("Make your guess: ").lower()
 
+    display = ""
+    if guess in guessed_letters:
+        print("You have guessed this letter before! akal ni haigi! lock in")
+    guessed_letters.append(guess)
+
+    if guess not in chosen_word:
+        lives -= 1
     for letter in chosen_word:
-        placeholder += "_"
-        if letter in display:
-            pass
-        elif letter == guess:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+            if guess in guessed_letters:
+                print("You have guessed this letter before! akal ni haigi! lock in")
+        elif letter in correct_letters:
             display += letter
         else:
-           display += "_"
-    print(placeholder)
+            display += "_"
+    if lives == 0:
+        is_game_over = True
+        print("you loose!")
+
+    print(stages[::-1][lives])
     print(display)
+    print(f"You have {lives} left! Singh lock in appa khalistan banona")
 
 
-# TODO-2: Change the for loop so that you keep the previous correct letters in display.
 
 
+
+
+# TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+# TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+#  e.g. You guessed d, that's not in the word. You lose a life.
