2f07fa1fab971927821468e0c272284f350ac7fb
DhaIiwal <edhaliwal@gobindsarvar.ca>
2026-03-30 11:21:56 -0700

    name

--- hangman/Hang_Wazir_Khan.py
+++ hangman/Hang_Wazir_Khan.py
@@ -5,6 +5,12 @@ from hangman_ARt import stages, logo
 print(logo)
 lives = 6
 chosen_word = random.choice(word_list)
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
 
 correct_letters =[]
 guessed_letters = []
@@ -12,7 +18,6 @@ guessed_letters = []
 is_game_over = False
 while not is_game_over:
     guess = input("Make your guess: ").lower()
-
     display = ""
     if guess in guessed_letters:
         print("You have guessed this letter before! akal ni haigi! lock in")
@@ -32,7 +37,10 @@ while not is_game_over:
             display += "_"
     if lives == 0:
         is_game_over = True
+        print(chosen_word)
         print("you loose!")
+    if "_" not in display:
+        print("YAYAYAYAYA YOU WINNNN")
 
     print(stages[::-1][lives])
     print(display)
@@ -43,6 +51,6 @@ while not is_game_over:
 
 
 
-# TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
 # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
 #  e.g. You guessed d, that's not in the word. You lose a life.
